3e4d427b1d59c5efa4b1e8dfa9458fe682b4cb52
kkawai <kkawai2@gmail.com>
2023-11-20 18:29:35 -0800

    minor fix imports

--- app/src/main/java/com/kk/android/coinprice/ui/coindetail/CoinDetailScreen.kt
+++ app/src/main/java/com/kk/android/coinprice/ui/coindetail/CoinDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.kk.android.coinprice.ui.coindetail
 
-import android.webkit.WebView
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
